d2ca68d9a9fbd4e242c8069715eb30a6157779f5
Andrii Fesych <andriyhimself@gmail.com>
2025-04-15 22:30:22 +0200

    init

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+This is an example of a simple landing page for Museum I build in 2022.
+It`s purpose is to promote Museum events, exhibitions, allow visitors to contact Museum, and follow their feed.
+
+[Live preview (DEMO LINK)](https://andriy-fesych.github.io/Museum_landing/)
+
+📌 Key technologies and frameworks used in the project:<br/>
+⭐ HTML<br/>
+⭐ SCSS<br/>
+📝 JavaScript (Vanilla)<br/>
+
+Follow these steps to set up this project locally:<br/>
+1. Clone the repo<br/>
+git clone https://github.com/andriy-fesych/Museum_landing.git<br/>
+2. This project was build on node version 14, but currently running on v20<br/>
+If You are using node version 20+, skip step 3. Otherwise, to easily switch between node versions I recommend using NVM:<br/>
+[Node Version Manager install guide](https://www.freecodecamp.org/news/node-version-manager-nvm-install-guide/)<br/>
+Using this guide ☝️ choose NVM v1.1.12 [from here](https://github.com/coreybutler/nvm-windows/releases/tag/1.1.12), as it is more stable<br/>
+3. Now to use correct node version:<br/>
+nvm install 20.19.0<br/>
+nvm use 20.19.0<br/>
+4. Install dependencies:<br/>
+npm install<br/>
+or<br/>
+yarn install<br/>
+5. Run the project locally:<br/>
+npm start<br/>
+or<br/>
+yarn start<br/>
+6. Congratulations! You are now set! 😊<br/>
+
+
+Features:<br/>
+⭐ Multiple layouts for phone, tablet and desktop resolutions.<br/>
+🎨 Smooth animations (SCSS + JS)<br/>
+⚡ Hover-sensitive for better UX<br/>
+🎚️ Slider in the tablet version is made without any pre-made solutions, on pure JS.<br/>
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,103 @@
 'use strict';
+
+const sidebar = document.body.querySelector('.sidebar');
+const burgerIcon = document.body.querySelector('.header__burgerIcon');
+const burgerIconImg = document.body.querySelector('.header__burgerIconImg');
+
+// const headerTitle = document.body.querySelector('.header__title');
+// ([burgerIcon, burgerIconImg, sidebar].includes(event.target))
+// (event.target === burgerIcon || event.target === sidebar)
+
+let slideIndex = 1;
+let timer;
+
+showSlides(slideIndex);
+
+// Next/previous controls
+/* function plusSlides(n) {
+  showSlides(slideIndex += n);
+} */
+
+// Thumbnail image controls
+const currentSlide = (n) => {
+  showSlides(n);
+};
+
+function showSlides(n) {
+  slideIndex = n;
+
+  let i;
+  const slides = document.querySelectorAll('.mySlides');
+  const dots = document.querySelectorAll('.dot');
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('active');
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+
+  // ADDED FOR IPAD
+  if ((window.innerWidth > 690) && (slideIndex < slides.length)) {
+    slides[slideIndex].style.display = 'block';
+  }
+  // REMOVE ABOVE IF NECESSARY
+
+  dots[slideIndex - 1].classList.add('active');
+
+  clearTimeout(timer);
+  timer = setTimeout(() => currentSlide(slideIndex + 1), 5000);
+}
+
+document.body.addEventListener('click', (event) => {
+/*   console.log('mouse click on ', event.target);
+  console.log(`current slide index ${slideIndex}`); */
+
+  // console.log(`The viewport's width is ${width}
+  // and the height is ${height}.`);
+
+  if (sidebar.classList.contains('active')) {
+    sidebar.classList.remove('active');
+  } else if ([burgerIcon, burgerIconImg, sidebar].includes(event.target)
+  && !sidebar.classList.contains('active')) {
+    // console.log('clicked on burger');
+
+    sidebar.classList.toggle('active');
+  }
+
+  if (event.target.classList.contains('dot')
+    || event.target.parentElement.classList.contains('dot')) {
+    // console.log('clicked on dot');
+
+    if (event.target.classList.contains('dot1')
+      || event.target.parentElement.classList.contains('dot1')) {
+      currentSlide(1);
+    }
+
+    if (event.target.classList.contains('dot2')
+      || event.target.parentElement.classList.contains('dot2')) {
+      currentSlide(2);
+    }
+
+    if (event.target.classList.contains('dot3')
+    || event.target.parentElement.classList.contains('dot3')) {
+      currentSlide(3);
+    }
+
+    if (event.target.classList.contains('dot4')
+      || event.target.parentElement.classList.contains('dot4')) {
+      currentSlide(4);
+    }
+  }
+});
